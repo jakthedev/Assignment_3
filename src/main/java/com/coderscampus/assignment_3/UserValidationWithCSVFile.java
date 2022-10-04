386190c49ae54f88173aba01f9e0dd95631e7885
JD <jdakemp@gmail.com>
2022-10-04 17:58:13 -0400

    First local set up commit

---
+++ src/main/java/com/coderscampus/assignment_3/UserValidationWithCSVFile.java
@@ -0,0 +1,11 @@
+package com.coderscampus.assignment_3;
+
+public class UserValidationWithCSVFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloe world ");
+
+	}
+
+}
