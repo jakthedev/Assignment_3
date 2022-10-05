087e0971af0a42361ef68664c20af9414c957b5a
JD <jdakemp@gmail.com>
2022-10-05 12:29:09 -0400

    Still working on the file extraxtion data and puting it into a String Array

--- src/main/java/com/coderscampus/assignment_3/UserValidationWithCSVFile.java
+++ src/main/java/com/coderscampus/assignment_3/UserValidationWithCSVFile.java
@@ -1,11 +1,19 @@
 package com.coderscampus.assignment_3;
 
+import com.coderscampus.myapp.service.UserService;
+
 public class UserValidationWithCSVFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Helloe world ");
+		UserService userservice = new UserService(); 
+		
+		User[] users = new User[0];
 
+		for (int i = 0; i < 0; i++) {
+		users[i] = userservice.createUser("trevor@craftycodr.com", "test123", "trevor");
+		
+		}
+		System.out.println(users);
 	}
 
 }
